a1861abf79d7fcce88c6aea31c4cfb2c07166462
David <ddropping@gmail.com>
2020-12-17 14:02:17 -0800

    Update project init

--- Questionnaire/Questionnaire/settings.py
+++ Questionnaire/Questionnaire/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_frameowrk_authtoken',
+    'rest_framework.authtoken',
 
     'users',
 ]
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = "users.CustomUser"
